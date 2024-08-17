462fce613f35af9c07c73aa6fdd776ea6bb2a6cd
Cipherexist <cipherexist@gmail.com>
2024-08-17 12:08:01 +0800

    sacewd

---
+++ registration/dbconfig.php
@@ -0,0 +1,25 @@
+<?php 
+
+$sqlhost = 'localhost';
+
+//$sqlhost = 'localhost'; 
+
+
+
+//$mysql_user  = 'navigato_admin';
+
+//$mysql_pass = 'N@vigator00000';
+
+//$mysql_db = 'navigato_gpnmonitor';	
+
+$mysql_user  = 'root';
+
+$mysql_pass = '';
+
+$mysql_db = 'mxbviwbu_invictus_review';			
+
+$con = mysqli_connect($sqlhost, $mysql_user , $mysql_pass, $mysql_db) or die('connection server error');
+
+
+
+?>
---
+++ registration/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enrollment</title>
+
+
+    
+    <!-- Bootstrap core CSS -->
+
+    <!-- Custom styles for this template -->
+    
+    <!--
+    <link href="css/theme.css" rel="stylesheet">
+   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   	<link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+   	-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css">
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.1/umd/popper.min.js"></script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script> 	
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/dataTables.bootstrap4.min.js"></script>	
+        
+        
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+
+</head>
+<body>
+
+<div class="container" style="padding-left: 30px;">
+
+<div class="row" style="margin-top: 20px">
+    <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Enrollment</a></li>
+        <li class="breadcrumb-item active">Information</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Payment</li>
+    </ol>
+    </nav>
+</div>
+
+    <div class="row" id="page1">
+
+    <h3>page 1</h3>
+
+
+    </div>
+
+    <div class="row" id="page2">
+
+    <h3>page 2</h3>
+
+
+    </div>
+
+    <div class="row" id="page3">
+
+    <h3>page 2</h3>
+
+
+    </div>
+
+
+    <div class="row">
+        <button type="button" class="btn btn-primary">Next</button>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+    
+</body>
+</html>
